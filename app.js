3e36b955a10b7b1a29e2e388b5133809806d4496
Windyl Monton <windyl1.monton.lvhs@gmail.com>
2025-06-26 16:42:30 +0800

    Initialized setup and connected to database

---
+++ app.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+//*detects changes in the database
+const db = mongoose.connection;
+
+mongoose.connect(process.env.DATABASE_URL);
+
+db.on("error", (err) => console.error("Error While connecting", err.message));
+db.on("open", () => console.log("Connected to Database"));
+
+app.use(express.json());
+
+app.listen(3000, () => {
+  console.log("Server Started");
+});
